3ff25c39df39eef3feea3fbfcd4dc819b0c36fc1
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-06-16 10:43:47 +0530

    Updating with employee attendance

--- src/empWageComputation/EmployeeWageMain.java
+++ src/empWageComputation/EmployeeWageMain.java
@@ -1,8 +1,19 @@
 package empWageComputation;
 
+import java.util.Random;
+
 public class EmployeeWageMain {
 
 	public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-    }
+        
+        Random random = new Random();
+        int attendance = random.nextInt(2); // 0 for Absent, 1 for Present
+
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }  
+	}
 }
