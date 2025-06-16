805b8e7c0a5e1ac4757bc67b7a4ef77f61bfc304
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-06-16 12:49:48 +0530

    Calculating wages for a month

--- src/empWageComputation/EmployeeWageMain.java
+++ src/empWageComputation/EmployeeWageMain.java
@@ -4,45 +4,39 @@ import java.util.Random;
 
 public class EmployeeWageMain {
 	
-	public static final int WAGE_PER_HOUR = 20;
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
+	    
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int WORKING_DAYS_PER_MONTH = 20;
 
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        
+
+        int totalWage = 0;
         Random random = new Random();
 
-        int attendance = random.nextInt(2); // 0 or 1
-        if (attendance == 0) {
-            System.out.println("Employee is Absent");
-            System.out.println("Employee Daily Wage: ₹0");
-            return;
+        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+            int empHours = 0;
+            int empCheck = random.nextInt(3); // 0, 1, 2
+
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                default:
+                    empHours = 0;
+            }
+
+            int dailyWage = empHours * WAGE_PER_HOUR;
+            totalWage += dailyWage;
+
+            System.out.println("Day " + day + ": Employee Hours = " + empHours + ", Daily Wage = ₹" + dailyWage);
         }
 
-        System.out.println("Employee is Present");
-
-        int empType = random.nextInt(3); // 0, 1, or 2
-
-        int empHours = 0;
-        switch (empType) {
-            case FULL_TIME:
-                empHours = 8;
-                System.out.println("Employee is Full-Time");
-                break;
-            case PART_TIME:
-                empHours = 4;
-                System.out.println("Employee is Part-Time");
-                break;
-            default:
-                empHours = 0;
-                System.out.println("Invalid Employee Type");
-                break;
-        }
-
-        int dailyWage = empHours * WAGE_PER_HOUR;
-        System.out.println("Employee Daily Wage: ₹" + dailyWage);
-    
+        System.out.println("Total Monthly Wage for 20 Days: ₹" + totalWage);
     }
 }
