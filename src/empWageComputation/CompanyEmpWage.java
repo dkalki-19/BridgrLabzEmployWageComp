59c6ec1bed54e6c537525ad9ff9970dae238f6d5
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-06-17 14:39:51 +0530

    Storing daily wage along with total wage

--- src/empWageComputation/CompanyEmpWage.java
+++ src/empWageComputation/CompanyEmpWage.java
@@ -1,26 +1,32 @@
 package empWageComputation;
 
+import java.util.ArrayList;
+
 public class CompanyEmpWage {
-    public final String company;
-    public final int wagePerHour;
-    public final int maxWorkingDays;
-    public final int maxWorkingHours;
-    public int totalWage;
+	 public final String company;
+	    public final int wagePerHour;
+	    public final int maxWorkingDays;
+	    public final int maxWorkingHours;
+	    public int totalWage;
+	    public ArrayList<Integer> dailyWages;
 
-    public CompanyEmpWage(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
-        this.company = company;
-        this.wagePerHour = wagePerHour;
-        this.maxWorkingDays = maxWorkingDays;
-        this.maxWorkingHours = maxWorkingHours;
-        this.totalWage = 0;
-    }
+	    public CompanyEmpWage(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+	        this.company = company;
+	        this.wagePerHour = wagePerHour;
+	        this.maxWorkingDays = maxWorkingDays;
+	        this.maxWorkingHours = maxWorkingHours;
+	        this.dailyWages = new ArrayList<>();
+	    }
 
-    public void setTotalWage(int totalWage) {
-        this.totalWage = totalWage;
-    }
+	    public void setTotalWage(int totalWage) {
+	        this.totalWage = totalWage;
+	    }
 
-    @Override
-    public String toString() {
-        return "Total Wage for Company " + company + " is ₹" + totalWage;
-    }
+	    @Override
+	    public String toString() {
+	        return "Company: " + company +
+	               "\nTotal Wage: ₹" + totalWage +
+	               "\nDaily Wages: " + dailyWages + "\n";
+	    }
+
 }
--- src/empWageComputation/EmployeeWageMain.java
+++ src/empWageComputation/EmployeeWageMain.java
@@ -7,76 +7,75 @@ import java.util.Random;
 
 public class EmployeeWageMain implements IEmployeeWageComputation{
 
-	 public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
-	    private final ArrayList<CompanyEmpWage> companyList;
+    private final ArrayList<CompanyEmpWage> companyList;
 
-	    public EmployeeWageMain() {
-	        companyList = new ArrayList<>();
-	    }
+    public EmployeeWageMain() {
+        companyList = new ArrayList<>();
+    }
 
-	    @Override
-	    public void addCompanyEmpWage(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
-	        companyList.add(new CompanyEmpWage(company, wagePerHour, maxWorkingDays, maxWorkingHours));
-	    }
+    @Override
+    public void addCompanyEmpWage(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        companyList.add(new CompanyEmpWage(company, wagePerHour, maxWorkingDays, maxWorkingHours));
+    }
 
-	    @Override
-	    public void computeEmployeeWage() {
-	        for (CompanyEmpWage company : companyList) {
-	            int totalWage = computeWage(company);
-	            company.setTotalWage(totalWage);
-	            System.out.println(company);
-	        }
-	    }
+    @Override
+    public void computeEmployeeWage() {
+        for (CompanyEmpWage company : companyList) {
+            int totalWage = computeWage(company);
+            company.setTotalWage(totalWage);
+            System.out.println(company);
+        }
+    }
 
-	    private int computeWage(CompanyEmpWage company) {
-	        int totalHours = 0;
-	        int totalDays = 0;
-	        int totalWage = 0;
-	        Random random = new Random();
+    private int computeWage(CompanyEmpWage company) {
+        int totalHours = 0;
+        int totalDays = 0;
+        int totalWage = 0;
+        Random random = new Random();
 
-	        while (totalDays < company.maxWorkingDays && totalHours < company.maxWorkingHours) {
-	            totalDays++;
+        while (totalDays < company.maxWorkingDays && totalHours < company.maxWorkingHours) {
+            totalDays++;
+            int empHours = 0;
+            int empCheck = random.nextInt(3); // 0, 1, 2
 
-	            int empHours = 0;
-	            int empCheck = random.nextInt(3); // 0, 1, 2
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                default:
+                    empHours = 0;
+            }
 
-	            switch (empCheck) {
-	                case IS_PART_TIME:
-	                    empHours = 4;
-	                    break;
-	                case IS_FULL_TIME:
-	                    empHours = 8;
-	                    break;
-	                default:
-	                    empHours = 0;
-	            }
+            if (totalHours + empHours > company.maxWorkingHours) {
+                empHours = company.maxWorkingHours - totalHours;
+            }
 
-	            if (totalHours + empHours > company.maxWorkingHours) {
-	                empHours = company.maxWorkingHours - totalHours;
-	            }
+            totalHours += empHours;
+            int dailyWage = empHours * company.wagePerHour;
+            company.dailyWages.add(dailyWage);
+            totalWage += dailyWage;
+        }
 
-	            totalHours += empHours;
-	            int dailyWage = empHours * company.wagePerHour;
-	            totalWage += dailyWage;
+        return totalWage;
+    }
 
-	            System.out.println(company.company + " - Day " + totalDays + ": Hours = " + empHours + ", Daily Wage = ₹" + dailyWage);
-	        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch\n");
 
-	        return totalWage;
-	    }
+        IEmployeeWageComputation empWageBuilder = new EmployeeWageMain();
+        empWageBuilder.addCompanyEmpWage("TCS", 20, 20, 100);
+        empWageBuilder.addCompanyEmpWage("Infosys", 25, 22, 120);
+        empWageBuilder.addCompanyEmpWage("Wipro", 18, 24, 90);
 
-	    public static void main(String[] args) {
-	        System.out.println("Welcome to Employee Wage Computation Program on Master Branch\n");
+        empWageBuilder.computeEmployeeWage();
+    }
 
-	        IEmployeeWageComputation empWageBuilder = new EmployeeWageMain();
-	        empWageBuilder.addCompanyEmpWage("TCS", 20, 20, 100);
-	        empWageBuilder.addCompanyEmpWage("Infosys", 25, 22, 120);
-	        empWageBuilder.addCompanyEmpWage("Wipro", 18, 24, 90);
-
-	        empWageBuilder.computeEmployeeWage();
-	    }
 }
 
 
